3309b1f520860550440ab19f770e8bcce8ca41c1
shalini <shalinijha812@gmail.com>
2018-10-11 23:10:17 +0530

    correct one

--- src/main/java/com/stackroute/movieservice/MovieServiceApplication.java
+++ src/main/java/com/stackroute/movieservice/MovieServiceApplication.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MovieServiceApplication implements  ApplicationListener<ContextRefreshedEvent>, CommandLineRunner {
 
